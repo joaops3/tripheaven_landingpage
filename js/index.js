cd1f4932a36d35b3a075b9df4319a1edf2192128
joao pedro <j.psilva3119@gmail.com>
2023-01-14 12:01:52 -0300

    add animations

--- js/index.js
+++ js/index.js
@@ -2,6 +2,10 @@ let menu = document.getElementById("hamburger-menu")
 let menu_content = document.getElementById("hamburger-content")
 let isOpen = false
 
+let header = document.getElementById('header')
+
+let content = document.querySelectorAll("[data-animate]")
+
 
 menu.addEventListener("click", () => {
     if(!isOpen){
@@ -11,4 +15,37 @@ menu.addEventListener("click", () => {
     }
     menu_content.classList.remove("active")
     isOpen = false
+})
+
+
+function activeAnimations(){
+    const windowTop = window.scrollY + window.innerHeight * 0.75
+    content.forEach((item)=> {
+        if(windowTop > item.offsetTop){
+            item.classList.add("animate")
+        }
+        // if(windowTop < item.offsetTop){
+        //     item.classList.remove("animate")
+        // }
+    })
+}
+
+
+window.addEventListener('scroll', ()=> {
+    if(window.scrollY > 400){
+        header.classList.remove("header-remove")
+        header.classList.add("header-active")
+    }
+    if(window.scrollY < 400){
+       
+        if(header.classList.contains("header-active")){
+
+            header.classList.add("header-remove")
+        }
+        header.classList.remove("header-active")
+    }
+    if(window.scrollY < 200 ){
+        header.classList.remove("header-remove")
+    }
+    activeAnimations()
 })
